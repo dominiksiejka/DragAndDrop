be5584750b11ac94d28505100a72abbc5739442d
sieja182 <dev@dominiksiejka.pl>
2021-07-27 18:20:10 +0200

    add to gh pages

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-import { ProjectInput } from "./components/project-input.js";
-import { ProjectList } from "./components/project-list.js";
-import { createModal } from "./util/modal.js";
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
-createModal();
-//# sourceMappingURL=app.js.map
