d8292a127d743931ad9a45cc42ce01ad03572330
sieja182 <dev@dominiksiejka.pl>
2021-07-27 17:13:54 +0200

    initialize project setup and files structure

---
+++ src/util/modal.ts
@@ -0,0 +1,17 @@
+export const createModal = (): void => {
+  const modal = document.createElement("div");
+  modal.id = "modal";
+  modal.innerHTML = `<div class="error-message">
+    <p>All input fields must be filled in order to add project !</p><button class="error-button">OK</button>
+</div>`;
+  modal.addEventListener("click", (event: any) => {
+    const clickedEl = event.target!;
+    if (
+      clickedEl.id === "modal" ||
+      clickedEl.classList.contains("error-button")
+    ) {
+      modal.classList.remove("hide");
+    } else return;
+  });
+  document.body.appendChild(modal);
+};
